8d7787ce60eb4269363f00673381f65788ecdb3d
Carlos Henrique <carloshenrique.pdoc@gmail.com>
2026-07-03 16:02:30 -0300

    cask: adicionar verified: na url (audit fix)

--- Casks/harbofly.rb
+++ Casks/harbofly.rb
@@ -2,7 +2,8 @@ cask "harbofly" do
   version "1.0.0"
   sha256 "db59df462fd2997ca0c03477d89771a654110e33d63a205db909aaad36f7610e"
 
-  url "https://github.com/carloshpdoc/Harbofly/releases/download/v#{version}/Harbofly.dmg"
+  url "https://github.com/carloshpdoc/Harbofly/releases/download/v#{version}/Harbofly.dmg",
+      verified: "github.com/carloshpdoc/Harbofly/"
   name "Harbofly"
   desc "Menu bar app that reclaims disk space from dev build artifacts and caches"
   homepage "https://harbofly.app/"
